89868668507364b15a8516d28b7b591a0f559fae
Siamak Ahmadi` <mrsiamak.dev@gmail.com>
2023-09-03 18:49:53 +0330

    Resolve Fonts

--- .history/src/app/layout_20230903183820.js
+++ .history/src/app/layout_20230903183820.js
@@ -1,5 +1,6 @@
 import './globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Aggeregator Admin',
@@ -9,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
