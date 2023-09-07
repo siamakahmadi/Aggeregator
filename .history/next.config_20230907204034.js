42404e435222c2c0695f2244d80442922660c684
Siamak Ahmadi` <mrsiamak.dev@gmail.com>
2023-09-07 22:18:14 +0330

    Add Category

---
+++ .history/next.config_20230907204034.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = {
+  experimental: {
+    serverActions: true,
+  },
+}
---
+++ .history/src/app/dashboard/components/Input/index_20230907200318.js
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useState } from 'react'
+import Styles from './style.module.scss'
+
+export default function Input(props, { onChange }) {
+  const [value, setValue] = useState('');
+
+
+  return (
+    <div className={Styles.inputContainer}>
+      <div className={Styles.inputTitle}>
+        {props.title}
+        {props.inputValue}
+      </div>
+      <div className={Styles.input} >
+        <input placeholder={props.placeholder}  />
+      </div>
+    </div>
+  );
+}
+
