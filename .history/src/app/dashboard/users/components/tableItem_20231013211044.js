c260f2beba5464302b4d7d40af9ee9892ef5c3d3
Siamak Ahmadi` <mrsiamak.dev@gmail.com>
2023-10-15 11:25:06 +0330

    New version 0.9.4

---
+++ .history/src/app/dashboard/users/components/tableItem_20231013211044.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Styles from './tableItem.module.scss'
+import StatusLabel from '../../components/StatusLabel/index'
+import Link from 'next/link'
+export default function tableItem(props) {
+    import Http from '../../../../../Axios/Https'
+    console.log(props.id)
+    function handleSusspend(id){
+        https.get(`admin/user/action/${id}`).then(
+          Response => {
+              setUsers(Response.data.data.data),
+              toast('Users successfully susspended');
+          }
+        ).catch(error => {
+          toast(`we cant sesspended User ${error}`);
+        })
+      }
+    return (
+        <div className={Styles.tableItems}>
+            <div className={Styles.profile}>
+                <div className={Styles.profileImage}>
+                    t
+                </div>
+                <div className={Styles.userName}>{props.name}</div>
+            </div>
+            <div className={Styles.email}>{props.email}</div>
+            <div className={Styles.date}>{props.date}</div>
+            <div>
+                <StatusLabel type={props.type} title={props.status} />
+            </div>
+            <div className={Styles.pointer}>
+                Suspend
+            </div>
+            <Link className={Styles.pointer} href={`${props.link}`}>
+                Detail
+            </Link>
+
+        </div>
+    )
+}
---
+++ .history/src/app/dashboard/users/components/tableItem_20231013211242.js
@@ -0,0 +1,36 @@
+'use client'
+import React from "react";
+import Styles from "./tableItem.module.scss";
+import StatusLabel from "../../components/StatusLabel/index";
+import Link from "next/link";
+export default function tableItem(props) {
+
+  function handleSusspend(id) {
+    https
+      .get(`admin/user/action/${id}`)
+      .then((Response) => {
+        setUsers(Response.data.data.data),
+          toast("Users successfully susspended");
+      })
+      .catch((error) => {
+        toast(`we cant sesspended User ${error}`);
+      });
+  }
+  return (
+    <div className={Styles.tableItems}>
+      <div className={Styles.profile}>
+        <div className={Styles.profileImage}>t</div>
+        <div className={Styles.userName}>{props.name}</div>
+      </div>
+      <div className={Styles.email}>{props.email}</div>
+      <div className={Styles.date}>{props.date}</div>
+      <div>
+        <StatusLabel type={props.type} title={props.status} />
+      </div>
+      <div className={Styles.pointer}>Suspend</div>
+      <Link className={Styles.pointer} href={`${props.link}`}>
+        Detail
+      </Link>
+    </div>
+  );
+}
