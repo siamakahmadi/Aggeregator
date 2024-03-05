05ffd5859861570370e2bfac5d4c340e87698e27
Siamak Ahmadi` <mrsiamak.dev@gmail.com>
2024-03-05 12:01:59 +0330

    New refines

---
+++ .history/src/app/users/content/[id]/page_20240304135204.js
@@ -0,0 +1,162 @@
+"use client";
+import { useContext, useState, useLayoutEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "../style.module.scss";
+import Btn from "../../Components/Btn/index";
+import CardLayout from "../../Components/HomeListLayout/index";
+import Card from "../../Components/Card/index";
+import ThemeContext from "../../Api/context/ThemeContext";
+import Loading from "../../Components/Loading/index";
+import Https from "../../Api/Https";
+// Icon List
+import RightFlash from "../../Assets/svg/RighFlash";
+import Tag from "../../Assets/svg/tag";
+import Typefaces from "../../Assets/svg/Typefaces";
+import Version from "../../Assets/svg/version";
+import Figma from "../../Assets/svg/figma";
+
+export default function Page({ params }) {
+  const https = new Https();
+  const theme = useContext(ThemeContext);
+  const [content, setcontent] = useState();
+  const [relatedPosts, setRelatedPosts] = useState();
+  const [pageLoading, setPageLoading] = useState([]);
+
+  console.log(content)
+
+  const versions = content && content.versions;
+
+  const sliderSettings = {
+    dots: true,
+    accessibility:true,
+    infinite: true,
+    speed: 500,
+    arrows:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  useLayoutEffect(() => {
+    https
+      .get(`user/post/${params.id}/show`)
+      .then((Response) => {
+        setcontent(Response.data.data.post_info),
+          setPageLoading(Response.data.message),
+          setRelatedPosts(Response.data.data.related_posts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const relatedPostsList = relatedPosts ? (
+    relatedPosts.map((item) => <Card key={item.index} title={item.title} />)
+  ) : (
+    <div>Related posts Not founds</div>
+  );
+
+  return (
+    <>
+      {pageLoading === "Post fetched" ? (
+        <main
+          className={
+            theme === "light" ? styles.contentDetail : styles.contentDetailDark
+          }
+        >
+          <div className={styles.header}>
+            <div className={styles.leftSide}>
+              <div className={styles.information}>
+                <div className={styles.title}>{content.title}</div>
+                <div className={styles.describe}>{content.description}</div>
+              </div>
+              <div className={styles.toolBar}>
+                <div className={styles.exploreBtn}>
+                  <Btn
+                    type="primary"
+                    hasIcon={true}
+                    link={content.website}
+                    title="Explore Site"
+                  >
+                    <RightFlash />
+                  </Btn>
+                </div>
+                <div className={styles.saveBtn}>
+                  <Btn type="secondary" link={content.website} title="Save">
+                    <RightFlash />
+                  </Btn>
+                </div>
+              </div>
+            </div>
+            <div className={styles.rightSide}>
+              <div className={styles.itemsList}>
+
+                {
+                  content.categories.map(category => (
+                    <div className={styles.item}>
+                    <div className={styles.icon}>
+                      {category.type==='Tags'?
+                      <Tag />:
+                      <Typefaces/>
+                      }
+                    </div>
+                    <div className={styles.text}>{category.name}</div>
+                  </div>
+                  ))
+                }
+
+              </div>
+            </div>
+          </div>
+          <div className={styles.versionDetails}>
+            <div className={styles.version}>
+              <div className={styles.icon}>
+                <Version />
+              </div>
+              
+            </div>
+            <div className={styles.figmaFile}>
+              <Figma />
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            {versions && (
+              <div className={styles.Slider}>
+                {versions.map((version) => (
+                  <Slider key={version.id} {...sliderSettings}>
+                    {version.files &&
+                      version.files.map((file) => (
+                        <img
+                          key={file.id}
+                          src={`https://radintechco.ir/echolab/public/storage/${file.address}.${file.extension}`}
+                          alt={file.original_name}
+                        />
+                      ))}
+                  </Slider>
+                ))}
+              </div>
+            )}
+          </div>
+          {content.anchor_website && <div className={styles.details}>
+            <div className={styles.avilable}>{content.anchor_website}</div>
+            <div className={styles.explore}>
+              <Btn  type="primary" hasIcon={true} title="Explore">
+                <RightFlash />
+              </Btn>
+            </div>
+          </div>}
+         
+          <div className={styles.suggestPosts}>
+            <div className={styles.title}>Related works</div>
+            <div className={styles.postlist}>
+              <CardLayout>{relatedPostsList}</CardLayout>
+            </div>
+          </div>
+        </main>
+      ) : (
+        <Loading title="Loading ... " />
+      )}
+    </>
+  );
+}
